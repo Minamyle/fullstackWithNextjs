62d32616e38373ba5354ce4dc7ba0b1084b67582
Amina Ismaila <minamyle@gmail.com>
2024-09-03 03:22:58 +0100

    fix:download of public folder error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="flexibble"
+          />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
